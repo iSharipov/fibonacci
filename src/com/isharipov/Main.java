47b6dfaf3d99138219cbaa75c0946e22e40b2ef8
ilias <iliamsharipov@gmail.com>
2017-09-03 19:25:59 +0300

    fib number

--- src/com/isharipov/Main.java
+++ src/com/isharipov/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        System.out.println(FibonacciUtils.getParallel(120));
+        System.out.println(FibonacciUtils.getParallel(80));
         System.out.println(System.currentTimeMillis() - start);
         start = System.currentTimeMillis();
         System.out.println(FibonacciUtils.getSeq(1000));
